b207a557c0fcd2d23b20e4561a412fb13d6133ca
CredStriker <149101396+CredStriker@users.noreply.github.com>
2024-03-12 10:16:49 +0530

    Create first.cpp

---
+++ first.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int a, b, c;
+cout<<"Prem Lal ------> 21BCS11722"<<endl;
+cout<<"Neter the two numbers to add"<<endl;
+cin>>a>>b;
+c=a+b;
+cout<<"The sum of"<<a<<"and"<<b<<"is"<<c<<endl;
+return 0:
+  }
